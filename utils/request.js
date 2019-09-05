4ecaeb74b9c33dfa3b112b0c280d31c209156889
aermotuo <aermotuo@163.com>
2019-09-05 18:23:12 +0800

    封装图片上传方法

--- utils/request.js
+++ utils/request.js
@@ -59,4 +59,30 @@ Request.prototype.post = function (url, data) {
   })
 }
 
+
+Request.prototype.uploadImage = function(images){
+  return new Promise((resolve, reject)=>{
+    var files = [];
+    var upload = function (images){
+      var file = arr.splice(0, 1)[0];
+      wx.uploadFile({
+        url: 'https://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
+        filePath: file,
+        name: 'file',
+        success(res) {
+          if(200){
+            files.push(res.data);
+            if (arr.length) {
+              upload(arr)
+            } else {
+              resolve(files);
+            }
+          }
+        }
+      })
+    }
+    
+  })
+}
+
 export default new Request();
